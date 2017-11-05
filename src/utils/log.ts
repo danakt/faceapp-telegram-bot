35a46809c361c98b760d11a7a46d5bc10f7b93f9
Danakt Frost <mail@danakt.ru>
2017-11-06 00:39:50 +0300

    fix showing log

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,11 +1,13 @@
 import { Message } from 'node-telegram-bot-api'
 
 /**
- * Send log in console 
- * @param msg 
+ * Send log in console
+ * @param msg
  */
 export function logRequest(msg: Message) {
   const formattedMessage: string = `
   Request by ${msg.from.first_name} ${msg.from.last_name} ${msg.from.username && '(@' + msg.from.username + ')'}
   ${msg.text}`
+
+  console.log(formattedMessage)
 }
