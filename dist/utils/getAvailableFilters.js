3c53ee3ff812a3cdf2ab4105221e05e5c2afa5ec
Danakt Frost <mail@danakt.ru>
2017-11-06 20:54:06 +0300

    discard ts compilation

--- dist/utils/getAvailableFilters.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var faceapp = require("faceapp");
-function getAvailableFilters() {
-    return faceapp.listFilters(true);
-}
-exports.default = getAvailableFilters;
--- dist/utils/log.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function logRequest(msg) {
-    var formattedMessage = "\n  Request by " + msg.from.first_name + " " + msg.from.last_name + " " + (msg.from.username && '(@' + msg.from.username + ')') + "\n  " + msg.text;
-    console.log(formattedMessage);
-}
-exports.logRequest = logRequest;
