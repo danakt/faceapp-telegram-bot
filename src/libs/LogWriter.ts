e4fbc686c5cb5a9119190cf82b173df48be66211
Danakt Frost <danakt_frost@mail.ru>
2018-01-22 01:08:27 +0300

    Add LogWriter class

---
+++ src/libs/LogWriter.ts
@@ -0,0 +1,104 @@
+import { createWriteStream, existsSync, mkdirSync, WriteStream } from 'fs'
+
+/**
+ * Class for writing logs to logfile
+ */
+export default class LogWritter {
+  /** Write streams */
+  private writeStreams: {
+    [key: string]: {
+      [dateString: string]: WriteStream
+    }
+  } = {}
+
+  /** Date of log writer in format yyyy-mm-dd */
+  private dateString: string
+
+  /**
+   * @param {string} dirName Directory of log files
+   */
+  public constructor(private dirName: string) {
+    // Making log dir
+    if (!existsSync(dirName)) {
+      mkdirSync(dirName)
+    }
+  }
+
+  /**
+   * Writes log to file
+   * @param {string} text Text to log
+   * @param {Level} level Level of log
+   */
+  public writeToLog(text: string, level: Level = 'info'): void {
+    const dateString: string = this.getDateString()
+
+    // Making new writestream if no exists
+    if (this.writeStreams[level] == null || this.writeStreams[level][dateString] == null) {
+      // Closing all streams
+      if (this.writeStreams[level] != null) {
+        const streamsList = this.writeStreams[level]
+
+        for (const key in streamsList) {
+          streamsList[key].close()
+        }
+      }
+
+      this.writeStreams[level] = {
+          [dateString]: createWriteStream(this.getFilePath(level), {
+          flags: 'a+',
+          encoding: 'utf8',
+          autoClose: true,
+        })
+      }
+    }
+
+    const writeStream: WriteStream = this.writeStreams[level][dateString]
+    writeStream.write(text.replace(/^\n^|^  /gm, '') + '\n\n')
+  }
+
+  /**
+   * Returns the file path of logfile
+   * @param {string} level Level of logging
+   * @return {string}
+   */
+  private getFilePath(level: Level): string {
+    const dateString: string = this.getDateString()
+    const filePath: string = `${this.dirName}/${level}-${dateString}.log`
+
+    return filePath
+  }
+
+  /**
+   * Adds zero berofe the two-digit number if needed, and returns string
+   * @param {number} num
+   * @return {string}
+   */
+  private addZeroBefore(num: number): string {
+    const strNum: string = num.toString()
+
+    return strNum.length === 1
+      ? '0' + strNum
+      : strNum
+  }
+
+  /**
+   * Rrturns date string in format yyyy-mm-dd
+   * @returns {string}
+   */
+  private getDateString(): string {
+    const date: Date = new Date()
+
+    const day: string = this.addZeroBefore(date.getDate())
+    const month: string = this.addZeroBefore(date.getMonth() + 1)
+    const year: string = date.getFullYear().toString()
+
+    const newDateString: string = `${year}-${month}-${day}`
+
+    // Updating currenct datestring
+    if (this.dateString !== newDateString) {
+      this.dateString = newDateString
+    }
+
+    return this.dateString
+  }
+}
