9fa39cb290e99f2b8872ec5587ad3f4792f4a83c
Danakt Frost <danakt_frost@mail.ru>
2018-01-21 22:12:09 +0300

    Update lang files

--- src/lang.ts
+++ src/lang.ts
@@ -3,13 +3,17 @@
  */
 export const locales = {
   en: {
-    START: 'Hello! I\'m Awesome FaceApp Bot. Just send me photo and ',
+    START: 'Hello! I\'m Awesome FaceApp Bot. Just send me photo and choose filter.',
     CHOOSE_FILTER: 'Choose the filter to apply to the photo',
     PHOTO_IS_PROCESSING: 'Please await, the photo is being processed...',
-},
+    NO_FACES: 'No faces found in photo',
+    UNKNOWN_ERROR: 'Unknown error',
+  },
   ru: {
-    START: 'Привет! Я Awesome FaceApp Bot. Просто отправите мне фото и выберите нужный фильтр.',
+    START: 'Привет! Я Awesome FaceApp Bot. Просто отправьте мне фото и выберите нужный фильтр.',
     CHOOSE_FILTER: 'Выберите фильтр для применения к фото',
-    PHOTO_IS_PROCESSING: 'Пожалуйста, подождите подождите, фото обрабатывается',
+    PHOTO_IS_PROCESSING: 'Пожалуйста, подождите, фото обрабатывается...',
+    NO_FACES: 'Не найдены лица на фотографии',
+    UNKNOWN_ERROR: 'Неизвестная ошибка',
   }
 }
